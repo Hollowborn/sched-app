f64ba35752c7eee393615e506226e916439d5e93
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-10-02 12:28:42 +0800

    added sonner on mount

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
-
+	import { Toaster, toast } from 'svelte-sonner';
 	let { children } = $props();
 </script>
 
+<Toaster />
 <svelte:head>
 	<link rel="icon" href={favicon} />
 </svelte:head>
