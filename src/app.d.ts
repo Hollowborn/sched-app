bc66079e343d28e1787f33677ba87ac21d2de043
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-10-05 16:49:50 +0800

    setting up SSR

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,18 @@
-import { SupabaseClient, Session, User } from '@supabase/supabase-js';
+import type { Session, SupabaseClient, User } from '@supabase/supabase-js';
+import type { Database } from './database.types.ts'; // import generated types
 
-// See https://kit.svelte.dev/docs/types#app
-// for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
 		interface Locals {
-			supabase: SupabaseClient;
+			supabase: SupabaseClient<Database>;
+			safeGetSession: () => Promise<{ session: Session | null; user: User | null }>;
 			session: Session | null;
 			user: User | null;
 		}
-		// interface PageData {}
+		interface PageData {
+			session: Session | null;
+		}
 		// interface PageState {}
 		// interface Platform {}
 	}
