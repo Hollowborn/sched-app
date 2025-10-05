03d1bd66530e9e868bccba83b703ad7102b524e0
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-10-05 16:51:09 +0800

    supabase ssr integration

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,23 @@
 	import '../app.css';
 	import favicon from '$lib/assets/favicon.svg';
 	import { Toaster } from 'svelte-sonner';
-	let { children } = $props();
+	import { ModeWatcher } from 'mode-watcher';
+
+	import { invalidate } from '$app/navigation';
+	import { onMount } from 'svelte';
+	let { data, children } = $props();
+	let { session, supabase } = $derived(data);
+	onMount(() => {
+		const { data } = supabase.auth.onAuthStateChange((_, newSession) => {
+			if (newSession?.expires_at !== session?.expires_at) {
+				invalidate('supabase:auth');
+			}
+		});
+		return () => data.subscription.unsubscribe();
+	});
 </script>
 
+<ModeWatcher />
 <Toaster />
 <svelte:head>
 	<link rel="icon" href={favicon} />
