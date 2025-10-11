e8823ebaad82b01c0d98305f2d81d163b1915243
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-10-11 18:42:16 +0800

    added a loading page

--- src/routes/menu/+layout.svelte
+++ src/routes/menu/+layout.svelte
@@ -3,7 +3,11 @@
 	import SiteHeader from '$lib/components/site-header.svelte';
 	import * as Sidebar from '$lib/components/ui/sidebar/index.js';
 	import type { ComponentProps } from 'svelte';
-
+	import { navigating } from '$app/stores';
+	import Skeleton from '$lib/components/ui/skeleton/skeleton.svelte';
+	import * as Empty from '$lib/components/ui/empty/index.js';
+	import { Spinner } from '$lib/components/ui/spinner/index.js';
+	import { Button } from '$lib/components/ui/button/index.js';
 	let { data, children, ...restProps } = $props<
 		{ data: any } & ComponentProps<typeof Sidebar.Root>
 	>();
@@ -22,7 +26,25 @@
 		<div class="flex flex-1 flex-col">
 			<div class="@container/main flex flex-1 flex-col gap-2">
 				<main class="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 md:px-10">
-					{@render children?.()}
+					{#if $navigating}
+						<div
+							class="absolute inset-0 z-50 flex flex-col items-center justify-center bg-background/80 backdrop-blur-sm"
+						>
+							<Empty.Root>
+								<Empty.Header>
+									<Empty.Media variant="icon">
+										<Spinner class="h-10 w-10" />
+									</Empty.Media>
+									<Empty.Title>Processing your request</Empty.Title>
+									<Empty.Description>
+										Please wait while we process your request. Do not refresh the page.
+									</Empty.Description>
+								</Empty.Header>
+							</Empty.Root>
+						</div>
+					{:else}
+						{@render children?.()}
+					{/if}
 				</main>
 			</div>
 		</div>
