863e032bb60a18622ef85a82facbbef499300c5c
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-11-08 00:24:15 +0800

    modified to use a boolean to identify the selected state using `selected`

--- src/lib/components/ui/table/table-row.svelte
+++ src/lib/components/ui/table/table-row.svelte
@@ -1,20 +1,24 @@
 <script lang="ts">
-	import { cn, type WithElementRef } from "$lib/utils.js";
-	import type { HTMLAttributes } from "svelte/elements";
+	import { cn, type WithElementRef } from '$lib/utils.js';
+	import type { HTMLAttributes } from 'svelte/elements';
 
 	let {
 		ref = $bindable(null),
 		class: className,
+		selected = false,
 		children,
 		...restProps
-	}: WithElementRef<HTMLAttributes<HTMLTableRowElement>> = $props();
+	}: WithElementRef<HTMLAttributes<HTMLTableRowElement>> & {
+		selected?: boolean;
+	} = $props();
 </script>
 
 <tr
 	bind:this={ref}
 	data-slot="table-row"
+	data-state={selected ? 'selected' : undefined}
 	class={cn(
-		"hover:[&,&>svelte-css-wrapper]:[&>th,td]:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors",
+		'hover:[&,&>svelte-css-wrapper]:[&>th,td]:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors',
 		className
 	)}
 	{...restProps}
