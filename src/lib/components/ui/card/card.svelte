c196d7259b13fe80d444bfbd3a13707a28264533
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-11-09 23:44:06 +0800

    --changed - modified light mode card-bg

--- src/lib/components/ui/card/card.svelte
+++ src/lib/components/ui/card/card.svelte
@@ -14,8 +14,8 @@
 	bind:this={ref}
 	data-slot="card"
 	class={cn(
-		'bg-card/40 text-card-foreground flex flex-col gap-6 rounded-xl py-6 backdrop-blur-md backdrop-saturate-150 relative',
-		'border border-white/20 dark:border-white/10',
+		'bg-card/30 text-card-foreground flex flex-col gap-6 rounded-xl py-6 backdrop-blur-md backdrop-saturate-150 relative',
+		'border border-white/10 dark:border-white/10',
 		'shadow-[0_0_0_1px_rgba(0,0,0,0.03),0_2px_4px_rgba(0,0,0,0.05),0_12px_24px_rgba(0,0,0,0.05)]',
 		'dark:shadow-[0_0_0_1px_rgba(255,255,255,0.05),0_2px_4px_rgba(0,0,0,0.1),0_12px_24px_rgba(0,0,0,0.1)]',
 		'before:absolute before:inset-0 before:-z-10 before:rounded-xl before:bg-gradient-to-br before:from-white/10 before:to-white/5 dark:before:from-white/5 dark:before:to-transparent',
