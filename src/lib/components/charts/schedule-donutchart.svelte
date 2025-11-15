231ecdc263ec5245d4e805f679a0e9267f3b2676
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-11-16 00:09:04 +0800

    added charts

---
+++ src/lib/components/charts/schedule-donutchart.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { PieChart } from 'layerchart';
+
+	type ScheduleStatusData = {
+		name: string;
+		value: number;
+	};
+
+	let { data } = $props<{ data: ScheduleStatusData[] }>();
+</script>
+
+<div class="chart-container">
+	<PieChart {data} label="name" value="value" innerRadius={60} legend={true} />
+</div>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 350px;
+	}
+</style>
---
+++ src/lib/components/charts/workload-barchart.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { BarChart } from 'layerchart';
+
+	type WorkloadData = {
+		name: string;
+		value: number;
+	};
+
+	let { data } = $props<{ data: WorkloadData[] }>();
+</script>
+
+<div class="chart-container">
+	<BarChart {data} x="name" y="value" legend />
+</div>
+
+<style>
+	.chart-container {
+		width: 100%;
+		height: 350px;
+	}
+</style>
