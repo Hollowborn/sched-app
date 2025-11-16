ff2f979b1651039bbe59b2355ac7858ae9fafcd3
FeignNext <102315773+Hollowborn@users.noreply.github.com>
2025-11-16 17:43:06 +0800

    removed @container class on div element to improve performance

--- src/routes/menu/+layout.svelte
+++ src/routes/menu/+layout.svelte
@@ -57,7 +57,7 @@
 		<SiteHeader />
 
 		<div class="flex flex-1 flex-col">
-			<div class="@container/main flex flex-1 flex-col gap-2">
+			<div class="flex flex-1 flex-col gap-2">
 				<main class="flex flex-col gap-4 py-4 md:gap-6 md:py-6 px-4 md:px-10">
 					{#if $navigating}
 						<!-- ... your skeleton loading state ... -->
